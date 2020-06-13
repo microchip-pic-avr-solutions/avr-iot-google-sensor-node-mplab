94a09438a31a2574a81477e45f3854a51087cddb
Microchip Technology <microchip@microchip.com>
2020-06-13 00:03:15 +0000

    pai-497 application sub module reference updated;

--- AVRIoT.X/mcc_generated_files/cli/cli.c
+++ AVRIoT.X/mcc_generated_files/cli/cli.c
@@ -73,7 +73,7 @@ static bool commandTooLongFlag = false;
 
 const char * const cli_version_number             = "3.0";
 
-const char * const firmware_version_number        = "3.0.0";
+const char * const firmware_version_number        = "4.0.0";
 
 static void command_received(char *command_text);
 static void reset_cmd(char *pArg);
