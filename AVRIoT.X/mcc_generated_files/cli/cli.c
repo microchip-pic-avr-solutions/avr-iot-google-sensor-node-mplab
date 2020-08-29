2a1eb2c5bbfbf7faddba9b3cbdba9eeacf6bf51d
Microchip Technology <microchip@microchip.com>
2020-08-29 00:17:51 +0000

    PAI-676 Updated the environment variable with the source URL of the repository.;

--- AVRIoT.X/mcc_generated_files/cli/cli.c
+++ AVRIoT.X/mcc_generated_files/cli/cli.c
@@ -71,7 +71,7 @@ static bool isCommandReceived = false;
 static uint8_t index = 0;
 static bool commandTooLongFlag = false;
 
-const char * const cli_version_number             = "3.0";
+const char * const cli_option_version_number      = "3.0.1";
 
 const char * const firmware_version_number        = "4.0.0";
 
@@ -336,7 +336,7 @@ static void get_device_id(char *pArg)
 static void get_cli_version(char *pArg)
 {
     (void)pArg;
-    printf("v%s\r\n\4", cli_version_number);
+    printf("v%s\r\n\4", cli_option_version_number);
 }
 
 static void get_firmware_version(char *pArg)
